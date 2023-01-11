fb7fff4fc7db950259d82ef59b06c149ff6c3fcc
Pingerock <pingerock@gmail.com>
2023-01-11 15:16:42 +0300

    Add project files.

---
+++ TextChanger/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace Text_Schizophrenia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.FileDialog_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.About_button = new System.Windows.Forms.Button();
+            this.Start_button = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FileDialog_button
+            // 
+            this.FileDialog_button.Location = new System.Drawing.Point(535, 14);
+            this.FileDialog_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.FileDialog_button.Name = "FileDialog_button";
+            this.FileDialog_button.Size = new System.Drawing.Size(139, 43);
+            this.FileDialog_button.TabIndex = 0;
+            this.FileDialog_button.Text = "Browse...";
+            this.FileDialog_button.UseVisualStyleBackColor = true;
+            this.FileDialog_button.Click += new System.EventHandler(this.FileDialog_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Specify the path to the text file (*.txt):";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 33);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(512, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Choose a replacement word";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(217, 69);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(172, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(395, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Number of replacements";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(555, 69);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(119, 22);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // About_button
+            // 
+            this.About_button.Location = new System.Drawing.Point(12, 402);
+            this.About_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.About_button.Name = "About_button";
+            this.About_button.Size = new System.Drawing.Size(101, 34);
+            this.About_button.TabIndex = 7;
+            this.About_button.Text = "Help";
+            this.About_button.UseVisualStyleBackColor = true;
+            this.About_button.Click += new System.EventHandler(this.About_button_Click);
+            // 
+            // Start_button
+            // 
+            this.Start_button.Location = new System.Drawing.Point(572, 402);
+            this.Start_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Start_button.Name = "Start_button";
+            this.Start_button.Size = new System.Drawing.Size(101, 34);
+            this.Start_button.TabIndex = 8;
+            this.Start_button.Text = "Start";
+            this.Start_button.UseVisualStyleBackColor = true;
+            this.Start_button.Click += new System.EventHandler(this.Start_button_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 97);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(661, 299);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(685, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Start_button);
+            this.Controls.Add(this.About_button);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FileDialog_button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Text changer";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FileDialog_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button About_button;
+        private System.Windows.Forms.Button Start_button;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ TextChanger/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Text_Schizophrenia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string str_file_name;
+        string word_for_change;
+        int countdown;
+        string[] words;
+
+        //Browse a text file
+        public void FileDialog_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text|*.txt|All|*.*";
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                str_file_name = openFileDialog.FileName;
+                textBox1.Text = str_file_name;
+            }
+        }
+
+        //Fills an array of strings with words from chosen text file before
+        public void split_text()
+        {
+            string text = "";
+            using (StreamReader fs = new StreamReader(str_file_name))
+            {
+                while (true)
+                {
+                    string temp = fs.ReadLine();
+                    if (temp == null)
+                    {
+                        break;
+                    }
+                    text += temp;
+                }
+            }
+            words = text.Split(' ');
+        }
+
+        //Choose a random word from the words array and replaces them with a replacement word
+        //until the countodwn reaches zero
+        public void Mix_it_up(int countdown, string word_for_change) {
+            Random rnd = new Random();
+            for (int x = countdown; x >= 0; x--)
+            {
+                int pointer;
+                do
+                {
+                    pointer = rnd.Next(1, words.Length - 2);
+                    if ((words[pointer] != word_for_change) &&
+                        (words[pointer-1] != word_for_change) &&
+                        (words[pointer+1] != word_for_change))
+                    {
+                        break;
+                    }
+                } while (words[pointer] != word_for_change);
+                words[pointer] = word_for_change;
+            }
+        }
+
+        //Creates a text file with replaced words and saves it in the desktop folder
+        public void Build_a_text_file(string[] words, int countdown)
+        {
+            string fileName = Path.GetFileName(str_file_name);
+            fileName = fileName.Remove(fileName.Length - 4);
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\" + fileName + "_Changed.txt";
+            FileInfo result_text_file = new FileInfo(filePath);
+            if (!result_text_file.Exists)
+            {
+                using (StreamWriter fs = result_text_file.CreateText())
+                {
+                    for(int x = 0; x < words.Length; x++)
+                    {
+                        fs.Write(words[x] + " ");
+                    }
+                }
+            } else
+            {
+                result_text_file.Delete();
+                using (StreamWriter fs = result_text_file.CreateText())
+                {
+                    for (int x = 0; x < words.Length; x++)
+                    {
+                        fs.Write(words[x] + " ");
+                    }
+                }
+            }
+        }
+
+        //Writes silly messages in the richTextBox while changing the text
+        public void Start_button_Click(object sender, EventArgs e)
+        {
+            richTextBox1.AppendText("\nCollecting data.\n");
+            richTextBox1.Refresh();
+            countdown = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+            if (countdown == 0)
+            {
+                MessageBox.Show("The number of replacements is zero.\n");
+                this.Close();
+            }
+            //These Sleep methods are just for a joke so people could read all messages in the richTextBox
+            Thread.Sleep(2000);
+            richTextBox1.Refresh();
+            split_text();
+            richTextBox1.AppendText("Preparing data for processing.\n");
+            Thread.Sleep(1000);
+            richTextBox1.Refresh();
+            if (words.Length < countdown) 
+            {
+                MessageBox.Show("The number of replacements cannot be greater than\nor equal to the number of words in the file.");
+                this.Close();
+            }
+            word_for_change = textBox2.Text;
+            Mix_it_up(countdown, word_for_change);
+            richTextBox1.AppendText("Changing words.\n");
+            Thread.Sleep(2000);
+            richTextBox1.Refresh();
+            Build_a_text_file(words, countdown);
+            richTextBox1.AppendText("Collecting the text in a new file.\n");
+            Thread.Sleep(2000);
+            richTextBox1.Refresh();
+            SystemSounds.Exclamation.Play();
+            richTextBox1.AppendText("All done! :)");
+        }
+
+        private void About_button_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Text changer \n" +
+                            "\n" +
+                            "Replaces random words in the text with the word you enter\n" + 
+                            "and displays them in a separate text file in the desktop folder.\n" +
+                            "\n" +
+                            "Made by Pingerock. \n" +
+                            "2021", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
